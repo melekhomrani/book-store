f723b03fce05582655ddd35e18fdb0d2502c8e36
melekhomrani <73800650+melekhomrani@users.noreply.github.com>
2022-02-24 23:57:18 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const indexRouter = require("./routers/index");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/BookStore", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    const port = process.env.port || 3000;
+    app.listen(port, () => console.log(`app is running on ${port}`));
+    console.log("connected to database...");
+  })
+  .catch((e) => console.log("couldn't connect to database..."));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(express.static("public"));
+app.use(expressLayout);
+app.use("/", indexRouter);
